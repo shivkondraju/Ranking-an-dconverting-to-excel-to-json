47ff0366a5c39b55bdb3a4ac8163c9c107b83215
Shiv Anand <82672285+shivkondraju@users.noreply.github.com>
2021-05-20 21:32:12 +0530

    Update ranking_excel_json.py

--- ranking_excel_json.py
+++ ranking_excel_json.py
@@ -1,1 +1,18 @@
 
+import pandas as pd
+import csv,json
+data=pd.read_excel('game_user_sample.xlsx',index_col=None)
+data['diff']=data['games_played']-data['games_won']
+data['rank']=data['diff'].rank(method='first',ascending=True)
+data=data.sort_values('rank')
+
+data.to_csv('out.csv',index=False)
+dataJson={}
+with open('out.csv') as csvFile:
+  csvReader=csv.DictReader(csvFile)
+  for csvRow in csvReader:
+    user_name=csvRow['user_name']
+    dataJson[user_name]=csvRow
+
+with open('jsonFile.json','w') as jsonFile:
+  jsonFile.write(json.dumps(dataJson,indent=4))
